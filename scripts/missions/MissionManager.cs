c637e53bc3c36bb4e0d46f7d304ddf9aa897003e
pashudzu <pavelcrosot@gmail.com>
2025-03-29 17:38:21 +0300

    Feat: add pop-up ads
    A new mission appears with an ad.

--- scripts/missions/MissionManager.cs
+++ scripts/missions/MissionManager.cs
@@ -4,12 +4,18 @@ using System.Linq;
 
 public partial class MissionManager : Node
 {
+	private TextureRect _popUpAd;
+	private Label _popUpLabel;
 	private Label _missionName;
 	private Label _missionDescription;
+	private AnimationPlayer animationPlayer;
 	private const string EmptyMissionText = "";
 	public override void _Ready() {
 		_missionName = GameManager.Instance.MissionNameLabel;
 		_missionDescription = GameManager.Instance.MissionDescriptionLabel;
+		_popUpAd = GetNode<TextureRect>("PopUpAd");
+		_popUpLabel = GetNode<Label>("PopUpAd/PopUpLabel");
+		animationPlayer = GetNode<AnimationPlayer>("PopUpAdAnimationPlayer");
 	}
 	public override void _Process(double delta) {
 		if (_missionName.Text == EmptyMissionText && _missionDescription.Text == EmptyMissionText) {
@@ -26,9 +32,13 @@ public partial class MissionManager : Node
 	private void SetMissionText(int _key) {
 		if (!Mission.MissionContains.Any()) {
 			GD.PrintErr("MissionContains не имеет значенией.");
-		} else if (_key >= 0 && _key < Mission.MissionContains.Count) {
+			return;
+		} 
+		if (_key >= 0 && _key < Mission.MissionContains.Count) {
 			_missionName.Text = Mission.MissionContains[_key].Name;
 			_missionDescription.Text = Mission.MissionContains[_key].Description;
+			_popUpLabel.Text = $"Новая миссия:\n{Mission.MissionContains[_key].Name} \n{Mission.MissionContains[_key].Description}";
+			animationPlayer.Play("advertisement");
 		} else {
 			GD.Print("Все миссии уже выполнены, или не проинициализированны.");
 		}
